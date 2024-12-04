80b5b0c514de3f5897f4f8c2c780be6cb8784be5
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2024-12-04 12:17:30 +0100

    v46

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -47,6 +47,17 @@ productRouter.put("/:id", isAuth, isAdmin, expressAsyncHandler(async (req, res) 
     }
 }));
 
+productRouter.delete("/:id", isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    
+    if(product){
+        await product.deleteOne();        
+        res.send({message:"Product Deleted"})
+    }else{
+        res.status(404).send({message:"Product Not Found"})
+    }
+}));
+
 const PAGE_SIZE = 3;
 
 productRouter.get("/admin", isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -31,8 +31,16 @@ const reducer = (state, action) => {
             return { ...state, loadingCreate: true }
         case 'CREATE_SUCCESS':
             return { ...state, loadingCreate: false }
-        case 'CREATE_FAIL¯':
+        case 'CREATE_FAIL':
             return { ...state, loadingCreate: false }
+        case 'DELETE_REQUEST':
+            return { ...state, loadingDelete: true, successDelete: false }
+        case 'DELETE_SUCCESS':
+            return { ...state, loadingDelete: false, successDelete:true }
+        case 'DELETE_FAIL':
+            return { ...state, loadingDelete: false, successDelete: false }
+            case 'DELETE_RESET':
+                return { ...state, loadingDelete: false, successDelete: false }  
         default:
             return state;
     }
@@ -49,7 +57,7 @@ function ProductListScreen() {
     const { state } = useContext(Store);
     const { userInfo } = state;
 
-    const [{ loading, error, products, pages, loadingCreate }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, products, pages, loadingCreate, loadingDelete, successDelete }, dispatch] = useReducer(reducer, {
         loading: true,
         error: ''
     });
@@ -68,8 +76,13 @@ function ProductListScreen() {
                 dispatch({ type: 'FETCH_FAIL', payload: getError(error) })
             }
         }
-        fetchData();
-    }, [page, userInfo]);
+        
+        if(successDelete){
+            dispatch({ type: 'DELETE_RESET' })
+        }else{
+            fetchData();
+        }
+    }, [page, userInfo, successDelete]);
 
     const createdHandler = async () => {
         if (window.confirm("Are you sure to create?")) {
@@ -89,6 +102,25 @@ function ProductListScreen() {
             }
         }
     };
+    const deleteHandler = async (product) => {
+        if (window.confirm("Are you sure to delete")) {
+            try {
+                dispatch({ type: "DELETE_REQUEST" });
+                await axios.delete(`/api/products/${product._id}`, {
+                    headers: {
+                        Authorization: `bearer ${userInfo.token}`
+                    }
+                });
+                dispatch({ type: "DELETE_SUCCESS" });
+                toast.success('The Product Deleted')
+                //navigate('/admin/products')
+
+            } catch (error) {
+                dispatch({ type: "DELETE_FAIL" });
+            }
+        }
+
+    }
     return (
         <div>
             <Helmet>
@@ -106,7 +138,10 @@ function ProductListScreen() {
                     </div>
                 </Col>
             </Row>
+
             {loadingCreate && <LoadingBox></LoadingBox>}
+            {loadingDelete && <LoadingBox></LoadingBox>}
+
             {loading ? (
                 <LoadingBox />
             ) : error ? (
@@ -134,11 +169,18 @@ function ProductListScreen() {
                                     <td>{product.brand}</td>
                                     <td>
                                         <Button
-                                        type='button'
-                                        variant='light'
-                                        onClick={()=>navigate(`/admin/product/${product._id}`)}
+                                            type='button'
+                                            variant='light'
+                                            onClick={() => navigate(`/admin/product/${product._id}`)}
                                         >
                                             Edit
+                                        </Button>{'  '}
+                                        <Button
+                                            type='button'
+                                            variant='light'
+                                            onClick={() => deleteHandler(product)}
+                                        >
+                                            Delete
                                         </Button>
                                     </td>
                                 </tr>
